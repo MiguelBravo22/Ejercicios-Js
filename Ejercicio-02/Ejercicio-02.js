ab44a71b105dfc448a16dd9cc5f0b756ad6668e2
MiguelBravo22 <124523446+MiguelBravo22@users.noreply.github.com>
2023-02-24 16:27:53 -0400

    Add files via upload

---
+++ Ejercicio-02/Ejercicio-02.js
@@ -0,0 +1,14 @@
+const Miguel = [
+    "Miguel Bravo",
+    24,
+    true,
+    new Date("aug 8 1998"),
+    libro = {
+        titulo: "El Principe",
+        autor: "Nicolas Maquiavelo",
+        fecha: new Date("1532"),
+        url: "https://acortar.link/hvwv3p"
+    }
+]
+
+console.log(Miguel)
